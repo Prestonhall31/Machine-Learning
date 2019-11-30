c65053c4c58fa736ea620a68590df8726a8c12f5
preston <prestonhall31@gmail.com>
2019-11-30 13:57:12 -0800

    Submitting project

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# coding: utf-8
 
 import sys
 import pickle
-from time import time
+import warnings
 sys.path.append("../tools/")
 
 # Local packages/files
@@ -13,19 +14,15 @@ from tester import dump_classifier_and_data
 import numpy as np
 import pandas as pd
 
-### Task 1: Select what features you'll use.
-### features_list is a list of strings, each of which is a feature name.
-### The first feature must be "poi".
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "rb") as data_file:
     data_dict = pickle.load(data_file)
 
-# import data_dict into a pandas dataframe for easier manipulation
-df = pd.DataFrame.from_dict(data_dict, orient = 'index')
 
 # import data_dict into a pandas dataframe for easier manipulation
 df = pd.DataFrame.from_dict(data_dict, orient = 'index')
+
 
 # Remove NaNs
 df = df.replace('NaN', np.nan)
@@ -33,20 +30,10 @@ df = df.replace('NaN', np.nan)
 # create a list of the employees
 names = pd.Series(list(data_dict.keys()))
 
-### Task 2: Remove outliers
+# Rows TOTAL and THE TRAVEL AGENCY IN THE PARK are not people and so will be removed from the dataset. 
+# The row 'LOCKHART EUGENE E' contains no information. 
+df = df.drop(['TOTAL', 'THE TRAVEL AGENCY IN THE PARK', 'LOCKHART EUGENE E'])
 
-# These names are not people so will be removed
-df = df.drop(['TOTAL', 'THE TRAVEL AGENCY IN THE PARK'])
-
-
-# separate and count the NA values in poi
-na_percentage = df.count() / len(df)
-na_percentage.sort_values(ascending=False)
-
-# separate and count the NA values in non_poi 
-non_poi_df = df[df.poi == False]
-
-# Removing these columns as there is more than 50% NaN's
 df = df.drop(['long_term_incentive', 
               'deferred_income', 
               'deferral_payments', 
@@ -56,7 +43,8 @@ df = df.drop(['long_term_incentive',
               'email_address'], axis=1)
 
 
-### Task 3: Create new feature(s)
+# ## Task 3: 
+# Create new feature(s)
 
 # Create and add features to the dataframe.
 df['from_poi_with_shared_receipt_percentage'] = df.apply(lambda row: float(row.from_poi_to_this_person / 
@@ -65,51 +53,50 @@ df['from_poi_with_shared_receipt_percentage'] = df.apply(lambda row: float(row.f
 df['total_compensation'] = df.apply(lambda row: float(row.total_payments + 
                                                       row.total_stock_value), axis=1)
 
-# Append features list
+
+
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
+# Setting emply features list for future use.
 features_list = []
+
+# Append features list
 for col in df.columns:
     features_list.append(col)
+
+# Move 'poi' to front of list
+features_list.insert(0, features_list.pop(features_list.index('poi')))
+
+
 
 ### Store to my_dataset for easy export below.
 my_dataset = df
 
+
 # Replacing the np.nan values with the string 'NaN' for preprocessing.
-my_dataset = my_dataset.replace(np.nan, 'NaN')
+my_dataset = my_dataset.replace(np.nan, 0)
 
 # Converting data back to dict for sklearn manipulation.
 my_dataset = my_dataset.to_dict('index')
 
-### Extract features and labels from dataset for local testing
-
-data = featureFormat(my_dataset, features_list, sort_keys = True)
-labels, features = targetFeatureSplit(data)
-
-### Use Sklearn's train_test_split model to separate data into testing and training data. 
-
-from sklearn.model_selection import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
-from sklearn import preprocessing
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import f_classif
+
+# from sklearn.feature_selection import SelectKBest, f_classif
+# # Create the object for SelectKBest and fit and transform the classification data
+# # k is the number of features you want to select [here it's 2]
+
+# X_clf_new=SelectKBest(score_func=f_classif,k=5).fit_transform(features,labels)
 
 
-min_max_scaler = preprocessing.MinMaxScaler()
-fieatures_minmax = min_max_scaler.fit_transform(features)
-
-selector = SelectKBest(score_func=f_classif, k=10)
-features_transformed = selector.fit_transform(fieatures_minmax, labels)
-
-### Choose the 5 best features for testing using SlectKBest
+### Choose the 5 best features for testing
 from sklearn.feature_selection import SelectKBest, f_classif
 
 clf = SelectKBest(f_classif, k=5)
-clf.fit_transform(features_train, labels_train)
+clf.fit_transform(features,labels)
 
 # Print the resulting labels
 k_best_lables = df.columns[clf.get_support(indices=True)]
@@ -120,65 +107,100 @@ labels_scores_df = pd.DataFrame(data = labels_scores, columns=['Feat_names', 'F_
 
 #Sort the dataframe for better visualization
 labels_scores_df_sorted = labels_scores_df.sort_values(['F_Scores', 'Feat_names'], ascending = [False, True])
+# print(labels_scores_df_sorted)
 
 
-### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
+### Use Sklearn's train_test_split model to separate data into testing and training data. 
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test =     train_test_split(features, labels, test_size=0.3, random_state=42)
 
-# GaussianNB
+
+## Naive Bayes
 from sklearn.naive_bayes import GaussianNB
+nb_clf = GaussianNB()
+
+## Decision Tree
+from sklearn import tree
+dt_clf = tree.DecisionTreeClassifier(min_samples_split=30)
+
+## Support Vector Machine Classifier
+from sklearn.svm import SVC
+svm_clf = SVC(gamma='scale', kernel='rbf', C=1000)
+
+## Adaboost Classifier
+from sklearn.ensemble import AdaBoostClassifier
+ab_clf = AdaBoostClassifier(algorithm='SAMME.R')
+
+## Random Forest
+from sklearn.ensemble import RandomForestClassifier
+rf_clf = RandomForestClassifier(n_estimators=10)
+
+
+from sklearn import metrics
 from sklearn.metrics import accuracy_score
 
-clf = GaussianNB()
-clf.fit(features_train, labels_train)    
-pred = clf.predict(features_test)
-acc = accuracy_score(pred, labels_test)
+
+clfs = [nb_clf, svm_clf, dt_clf, ab_clf, rf_clf]
 
 
-print("NB Accuracy: ", acc)
+def classifyer(clfs, f_train, f_test, l_train, l_test):
 
+    classifier_names = ['Naive Bayes', 'Support Vector Machine', 'Decision Tree', 'Ada Boost', 'Random Forest']
+    accuracy = []
+    precision = []
+    recall = []
 
-# Support Vector Machine Classifier
-from sklearn.svm import SVC
+    for classifier in [nb_clf, svm_clf, dt_clf, ab_clf, rf_clf]:
 
-clf = SVC(gamma='auto') # (kernel="rbf", C=10000, gamma='scale')
-clf.fit(features_train, labels_train)  
-pred = clf.predict(features_test) 
-accuracy = accuracy_score(pred, labels_test)
+        clf = classifier
+        clf.fit(f_train, l_train)
+        predictions = clf.predict(f_test)
 
-print("SVC Accuracy: ", accuracy)
-
-
-# Decision Tree
-from sklearn import tree
-
-clf = tree.DecisionTreeClassifier(min_samples_split=40)
-clf.fit(features_train, labels_train)  
-pred = clf.predict(features_test) 
-accuracy = accuracy_score(pred, labels_test)
-
-print("DT Accuracy: ", accuracy)
-
-
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
-
-# Example starting point. Try investigating other evaluation techniques!
+        accuracy.append("{0:.0%}".format(accuracy_score(l_test, predictions)))
+        precision.append("{0:.0%}".format(metrics.precision_score(l_test, predictions)))
+        recall.append("{0:.0%}".format(metrics.recall_score(l_test, predictions)))
+        
+    clf_df = pd.DataFrame([accuracy, precision, recall],
+                          columns = classifier_names,
+                          index=['Accuracy', 'Precision', 'Recall'])
+    
+    return clf_df
 
 
 
+classifyer(clfs, features_train, features_test, labels_train, labels_test)
 
 
-### Task 6: Dump your classifier, dataset, and features_list so anyone can
-### check your results. You do not need to change anything below, but make sure
-### that the version of poi_id.py that you submit can be run on its own and
-### generates the necessary .pkl files for validating your results.
+# ## Task 5: 
+# Tune your classifier to achieve better than .3 precision and recall using our testing script. Check the tester.py script in the final project folder for details on the evaluation method, especially the test_classifier function. Because of the small size of the dataset, the script uses stratified shuffle split cross validation. For more info: http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+# 
+
+
+######################################################################################################
+######################################################################################################
+
+
+
+# Using K-Fold to validate 
+
+from sklearn.model_selection import KFold
+from sklearn import metrics
+
+kf = KFold(n_splits = 4, shuffle = True)
+
+for train_indices, test_indices in kf.split(labels):
+    # make training and testing dataset
+    kfeatures_train = [features[ii] for ii in train_indices]
+    kfeatures_test = [features[ii] for ii in test_indices]
+    klabels_train = [labels[ii] for ii in train_indices]
+    klabels_test = [labels[ii] for ii in test_indices]
+
+
+classifyer(clfs, kfeatures_train, kfeatures_test, klabels_train, klabels_test)
+
+
+# ## Task 6: 
+# Dump your classifier, dataset, and features_list so anyone can check your results. You do not need to change anything below, but make sure that the version of poi_id.py that you submit can be run on its own and generates the necessary .pkl files for validating your results.
 
 dump_classifier_and_data(clf, my_dataset, features_list)
+
